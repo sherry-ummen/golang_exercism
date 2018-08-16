72deb49fc86a0d91a1b4f9cbdddf27dd49b746ba
Sherry Ummen <sherry.ummen@outlook.com>
2018-08-16 22:12:43 +0300

    Hello World

--- hello-world/hello_world.go
+++ hello-world/hello_world.go
@@ -3,7 +3,7 @@ Package greeting provides a method which returns a string Hello, World!
 */
 package greeting
 
-// HelloWorld Method retunrs a string Hello, World!
+// HelloWorld Method returns a string Hello, World!
 func HelloWorld() string {
 	return "Hello, World!"
 }
